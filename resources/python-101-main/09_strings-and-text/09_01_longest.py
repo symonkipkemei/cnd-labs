d1b73b66d1ae2a59b494f17e0bfcb5438d871f18
symonkipkemei <symonkipkemei@gmail.com>
2022-04-23 23:07:06 +0300

    updates

--- resources/python-101-main/09_strings-and-text/09_01_longest.py
+++ resources/python-101-main/09_strings-and-text/09_01_longest.py
@@ -5,3 +5,12 @@ longest_german_word = "Donaudampfschifffahrtsgesellschaftskapitänskajütentürs
 longest_hungarian_word = "Megszentségteleníthetetlenségeskedéseitekért"
 longest_finnish_word = "Lentokonesuihkuturbiinimoottoriapumekaanikkoaliupseerioppilas"
 strong_password = "%8Ddb^ca<*'{9pur/Y(8n}^QPm3G?JJY}\(<bCGHv^FfM}.;)khpkSYTfMA@>N"
+
+list_names = [len(longest_german_word), len(longest_hungarian_word), len(longest_finnish_word), len(strong_password)]
+print(list_names)
+list_names.sort(reverse= True)
+
+print(list_names)
+print("The largest is",list_names[0])
+
+
