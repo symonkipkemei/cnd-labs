cc187eb468309c2b7d7c8d5858c011b8e817f21a
symonkipkemei <symonkipkemei@gmail.com>
2022-05-02 10:08:16 +0300

    updates

--- projects/python-101-main/nothing.py
+++ projects/python-101-main/nothing.py
@@ -8,3 +8,37 @@ for x in list:
     # sijui cha kufanya
 
 print("tupatane mtihani")
+
+x=10
+
+
+print(f"I love naimnals {x}")
+
+print(type(x))
+
+
+height = 2.5
+width = 3
+area = width * height
+print(area)
+
+
+x = 9
+y = 4.5
+z = x / y
+print(type(z))
+
+
+# another example
+counter = 0
+sum = 0
+
+while (counter < 10):
+    sum += counter
+    print("sum",sum)
+    counter += 3
+    print("counter",counter)
+    print("\n")
+
+print(counter)
+print(sum)
--- resources/python-101-main/09_strings-and-text/09_01_longest.py
+++ resources/python-101-main/09_strings-and-text/09_01_longest.py
@@ -6,11 +6,13 @@ longest_hungarian_word = "Megszentségteleníthetetlenségeskedéseitekért"
 longest_finnish_word = "Lentokonesuihkuturbiinimoottoriapumekaanikkoaliupseerioppilas"
 strong_password = "%8Ddb^ca<*'{9pur/Y(8n}^QPm3G?JJY}\(<bCGHv^FfM}.;)khpkSYTfMA@>N"
 
-list_names = [len(longest_german_word), len(longest_hungarian_word), len(longest_finnish_word), len(strong_password)]
-print(list_names)
-list_names.sort(reverse= True)
 
-print(list_names)
-print("The largest is",list_names[0])
+# we can alternatively use python to help us establish which is the largest by use of dictionary
+# since thhe comparison is short we wil do it ourselves
+
+print (longest_finnish_word,len(longest_finnish_word) )
+print(longest_hungarian_word,len(longest_hungarian_word) )
+print (longest_german_word,len(longest_german_word) )
+print(strong_password, len(strong_password))
 
 
