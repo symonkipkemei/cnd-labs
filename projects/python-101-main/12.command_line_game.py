fcb232c45249c8a385c3e2d0fb8c2612546446f3
symonkipkemei <symonkipkemei@gmail.com>
2022-06-26 15:16:31 +0300

    the dragons game

--- projects/python-101-main/12.command_line_game.py
+++ projects/python-101-main/12.command_line_game.py
@@ -1,5 +1,8 @@
 #G0 GETTER SUB PROGRAMS
 
+
+#if the user happens to find the sword, assign variable to one
+sword = 0
 def player_name():
     """Collect user_name and welcome him/her to the game"""
     player_name = input("What's your name ? : ")
@@ -15,22 +18,82 @@ def door_choices():
 
     return door_choice
 
-def left_room():
+def left_door():
     print("Oops! You've entered an empty room")
 
-
+def right_door():
+    print("Booom !! I am the dragon, I am going to eat you !!")
+    
 
 def option():
-    """You have an option of intteracting further or return to previous room"""
+    """You have an option of interacting further or return to previous room"""
     print("\nYou have two options\n(1). Interact further\n(2). Run to the previous room")
     user_option = int(input("Make your choice: "))
 
     return user_option
 
-def sword_option():
-    """You have a sword, choose what to do with it."""
+def option_further_right():
+    print("\nYou have two options\n(1).fight\n(2).Run to the previous room")
+    user_option = int(input("Make your choice: "))
+    return user_option
+
+def option_further_left():
+    """Option further"""
+    print("\nYou have two options\n(1).Look around\n(2). Run to the previous room")
+    user_option = int(input("Make your choice: "))
+    if user_option == 1:
+        return user_option
+    elif user_option == 2:
+        return
+
+def sword_found():
+    """Sword found"""
+    print("\nYou have found a sword!\n(1).Take it\n(2). Leave it")
+    user_option = int(input("Make your choice: "))
+    if user_option == 1:
+        sword = 1
+        return sword
+    elif user_option == 2:
+        sword = 0
+        return sword
+
+def main():
+    sword = 0
+    player_name()
+    return_previous_room = True
+    while return_previous_room:
+        door_choice = door_choices()
+        if door_choice == 1:
+            left_door()
+            user_option = option()
+            if user_option == 1:
+                further_option = option_further_left()
+                if further_option == 1:
+                    sword = sword_found()
+                elif further_option == 2:
+                    return_previous_room = True
+                
+        elif door_choice == 2:
+            right_door()
+            user_option = option()
+            if user_option == 1:
+                further_option = option_further_right()
+                if further_option == 1:
+                    if sword == 1:
+                        print("You've won ! congratulations!")
+                    
+                    elif sword == 0:
+                        print("you've lost")
+                    return_previous_room = False
+
+                elif further_option == 2:
+                    return_previous_room = True
+        else:
+            print("Try again")
+
     
 
+main()
 
 
         
