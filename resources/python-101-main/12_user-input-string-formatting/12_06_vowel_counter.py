770a90b52ea45ce54ea02b7e7549c35383bf8dad
symonkipkemei <symonkipkemei@gmail.com>
2022-06-30 09:36:44 +0300

    which is greater?

--- resources/python-101-main/12_user-input-string-formatting/12_06_vowel_counter.py
+++ resources/python-101-main/12_user-input-string-formatting/12_06_vowel_counter.py
@@ -4,3 +4,12 @@
 
 #we will continue as from tomorrow.
 # learn to prioritise me when you start your day, I deserve better.I am your future. I can actually automate all these task you are struggling to do.
+
+
+user_input = input("Insert text: ")
+count = 0
+vowels = ["a", "e", "i", "o", "u"]
+for x in user_input:
+    if x in vowels:
+        count += 1
+print(count)
