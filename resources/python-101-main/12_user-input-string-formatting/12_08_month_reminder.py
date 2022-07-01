99ca9c033e8927dc3d56039801592120c78b1562
symonkipkemei <symonkipkemei@gmail.com>
2022-07-01 11:37:03 +0300

    Interesting

--- resources/python-101-main/12_user-input-string-formatting/12_08_month_reminder.py
+++ resources/python-101-main/12_user-input-string-formatting/12_08_month_reminder.py
@@ -3,3 +3,17 @@
 # "January", "February", ... "December"
 # Print "Error" if the number from the user is not between 1 and 12.
 # Use a nested `if` statement.
+
+
+
+user_number = int(input("Insert a number between 1 and 12: "))
+
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+
+if user_number <= 12 and user_number >= 1:
+    for index, x in enumerate(months):
+        if (index + 1) == user_number:
+            print( x , end=" ")
+else:
+    print('The selected number does not represent a month')
