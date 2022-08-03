1b05dccccc0519b8ed0e676456a72b76f312887c
symonkipkemei <symonkipkemei@gmail.com>
2022-08-03 22:34:45 +0300

    updates

---
+++ python-201/labs/list.py
@@ -0,0 +1,18 @@
+fruits= ["mango", "watermelon", "banana", "oranges", "carrots", "pears", "palms", "grapes"]
+
+
+
+print(fruits[::-1])
+
+
+print(fruits[5:])
+
+print(fruits[-1])
+for fruit in fruits:
+    print(fruit)
+
+
+
+bucket_list = ["rootop love", "sun bath", "go to Namibia desert", "Kill a gazelle", "impact the world", "influence people"]
+
+print(bucket_list[0:7:1])
---
+++ python-201/labs/tuples.py
@@ -0,0 +1,29 @@
+ 
+ 
+tupl = 1, 4, "symon"
+print(tupl)
+
+
+tuple1 = (1, 4, "symon")
+tuple2 = tuple1[:3]
+print(tuple2)
+
+
+great = tuple1 + tuple2
+
+name = "gueufbbnfkwehlhewfh"
+name= tuple(name)
+print(len(name))
+
+for element in great:
+    print(element)
+
+
+friends = ("winny", "kipchumba","langat", "mercy", "beverly", "materte", "agnes")
+
+friend = friends[1]
+print(friend[1])
+
+
+for friend in friends:
+    print(friend[-1])
