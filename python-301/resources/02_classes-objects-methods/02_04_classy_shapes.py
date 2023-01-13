12380147acf246a02ba9d218073e09d77d488f52
symonkipkemei <symonkipkemei@gmail.com>
2023-01-13 21:24:46 +0300

    combining areas with add dunder

--- python-301/resources/02_classes-objects-methods/02_04_classy_shapes.py
+++ python-301/resources/02_classes-objects-methods/02_04_classy_shapes.py
@@ -5,3 +5,45 @@
 # Write methods in the appropriate class so that you can calculate
 # the area of both the rectangle and the circle, the perimeter
 # of the rectangle, and the circumference of the circle.
+
+import math
+class Rectangle:
+    def __init__(self,length, width) -> None:
+        self.length = length
+        self.width = width
+        self.area = self.length * self.width
+        self.perimeter = 2 * (self.length + self.width)
+    
+    def __add__(self,other):
+        combined_area = self.area + other.area
+        return f"combined_area: {combined_area}"
+
+    
+
+class Circle:
+    def __init__(self,radius) -> None:
+        self.radius = radius
+        self.perimeter = 2 * math.pi * self.radius
+        self.area = math.pi * (self.radius * self.radius)
+
+
+    def __add__(self,other):
+        combined_area = self.area + other.area
+        return f"combined_area: {combined_area}"
+
+    
+
+ #instantiate an object
+rec = Rectangle(8,4)
+circle = Circle(14)
+
+print(circle.area)
+print(circle.perimeter)
+
+print(rec.perimeter)
+print(rec.area)
+
+ca = rec + circle
+cb = circle + rec
+print(ca)
+print(cb)
